703e85a070aab72a5f07d56a2428452c77e20b5e
lbCyber <paul@lintbox.com>
2020-08-30 09:38:26 -0400

    preventing default on submit, + small fixes

--- humantime.js
+++ humantime.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', () => {
   document.getElementById("oldTime").value = 0
   document.getElementById("humanTime").value = ""
   const formatDuration = (s) => {
     const t = [ // Array of times, 0 = value in seconds, 1 = written word, 2 = accumulator
-      [31536000000,"millennium",0],[3153600000,"century",0],[31536000, "year", 0], [86400, "day", 0], [3600, "hour", 0], [60, "minute", 0], [1, "second", 0]
+      [31536000000, "millennium", 0], [3153600000, "century", 0], [31536000, "year", 0], [86400, "day", 0], [3600, "hour", 0], [60, "minute", 0], [1, "second", 0]
     ]
     let tot = s // Total before s is manipulated
     let str = "" // String value for answer
@@ -12,12 +12,12 @@ document.addEventListener('DOMContentLoaded', function (event) {
       v[2] = val
       s -= val * v[0]
       if (val !== 1) {
-        if (k==0) {
+        if (k == 0) {
           v[1] = "millennia"
-        } else if (k==1) {
+        } else if (k == 1) {
           v[1] = "centuries"
         } else {
-        v[1] += "s"
+          v[1] += "s"
         }
       } // If there's more than one, pluralize the word
     })
@@ -33,9 +33,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
     })
     return tot == 0 ? `now` : str
   }
+  document.getElementById("oldTime").addEventListener("keypress", (e) => {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+      return false;
+    }
+  })
   document.getElementById("oldTime").addEventListener("input", (e) => {
-    e.preventDefault()
-    let oldTime = parseInt(document.getElementById("oldTime").value.replace(/\D/g,"")) + 0
+    let oldTime = parseInt(document.getElementById("oldTime").value.replace(/\D/g, "")) + 0
     document.getElementById("oldTime").value = oldTime
     let newTime = formatDuration(oldTime)
     document.getElementById("humanTime").value = newTime
